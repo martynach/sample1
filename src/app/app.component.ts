318f5419931c7d0d9921e67edae5266933ed60d0
Martyna <martyna.niegowska@motorolasolutions.com>
2024-01-13 18:15:19 +0100

    another title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Sample title2';
+  title = 'Sample title3';
 }
